8396e65fbc82a7e5bc7fad695ef21f46a876f3dc
Sarvesh Khire <37295002+khire12@users.noreply.github.com>
2021-01-31 18:04:19 +0530

    Adds test files 5,7,8

---
+++ test5.py
@@ -0,0 +1,21 @@
+import sys, random
+
+
+
+
+
+
+class Apple:
+    def __init__(self, color):
+        self.color = color
+
+    def repeat_color(self, num = 10):
+        for i in range(num):
+            print(self.color)   
+
+
+k=1
+apple = Apple("red")
+apple.repeat_color()
+
+
